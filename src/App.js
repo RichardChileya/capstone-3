a83e3421b5ce53968839d367905e00e949f5e4e0
RichardChileya <richardchileyawk@gmail.com>
2022-11-24 12:46:17 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './components/Home';
-import Details from './components/Details';
+import { fetchCountriesData } from './redux/countries/countries';
+import Header from './components/Header';
+import HomePage from './components/Home';
+import CountryInfo from './components/CountryInfo';
 
-const App = () => (
-  <div className="App">
-    <Navbar />
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/display" element={<Details />} />
-      </Routes>
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountriesData());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/countries/:countryName" element={<CountryInfo />} />
+        </Routes>
+      </main>
     </div>
-  </div>
-);
+  );
+}
 
 export default App;
